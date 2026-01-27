b77990a0002e010a31694ac8538c90369d8cd220
nguyenDucThang15 <116487174+nguyenDucThang15@users.noreply.github.com>
2026-01-27 10:17:21 +0700

    setup theme

--- eslint.config.js
+++ eslint.config.js
@@ -2,6 +2,7 @@ import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
+import importPlugin from 'eslint-plugin-import'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
@@ -13,6 +14,9 @@ export default defineConfig([
       reactHooks.configs.flat.recommended,
       reactRefresh.configs.vite,
     ],
+    plugins: {
+      import: importPlugin,
+    },
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
@@ -23,62 +27,32 @@ export default defineConfig([
       },
     },
     rules: {
-      /* =======================
-      * Core / Possible Errors
-      * ======================= */
+      //Core / Possible Errors
       'no-undef': 'error',
       'no-debugger': 'error',
       'eqeqeq': ['error', 'always'],
       'no-unused-vars': ['warn', { varsIgnorePattern: '^[A-Z_]' }],
       'no-console': ['warn', { allow: ['warn', 'error'] }],
 
-      /* =======================
-      * React / Hooks
-      * ======================= */
+      // React / Hooks
       'react-hooks/rules-of-hooks': 'error',
       'react-hooks/exhaustive-deps': 'warn',
 
-      /* =======================
-      * Imports
-      * ======================= */
-      'import/no-duplicates': 'error',
-      'import/order': [
-        'warn',
-        {
-          groups: [
-            'builtin',
-            'external',
-            'internal',
-            'parent',
-            'sibling',
-            'index',
-          ],
-          'newlines-between': 'always',
-        },
-      ],
-
-      /* =======================
-      * Style / Spacing
-      * ======================= */
+      // Style / Spacing
       'semi': ['warn', 'never'],
       'quotes': ['warn', 'single', { avoidEscape: true }],
       'array-bracket-spacing': ['warn', 'never'],
       'keyword-spacing': ['warn', { before: true, after: true }],
       'comma-spacing': ['warn', { before: false, after: true }],
-      'operator-spacing': ['warn', { before: true, after: true }],
       'no-multi-spaces': ['warn', { ignoreEOLComments: true }],
       'no-multiple-empty-lines': ['warn', { max: 1 }],
 
-      /* =======================
-      * Clean Code
-      * ======================= */
+      // Clean Code
       'prefer-const': 'warn',
       'no-var': 'error',
       'object-shorthand': 'warn',
 
-      /* =======================
-      * Safety / Best Practices
-      * ======================= */
+      // Safety / Best Practices
       'no-alert': 'warn',
     },
   },
