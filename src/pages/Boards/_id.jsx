ac012214d20c2c9ab648429a0aebae6e65ad149e
nguyenDucThang15 <116487174+nguyenDucThang15@users.noreply.github.com>
2026-03-06 09:14:50 +0700

    setup call api

--- src/pages/Boards/_id.jsx
+++ src/pages/Boards/_id.jsx
@@ -3,13 +3,25 @@ import AppBar from '~/components/AppBar/AppBar'
 import BoardBar from './BoardBar/BoardBar'
 import BoardContent from './BoardContent/BoardContent'
 import { mockData } from '~/apis/mock-data'
+import { useEffect, useState } from 'react'
+import { fetchBoardDetailsAPI } from '~/apis'
 
 const Board = () => {
+  const [board, setBoard] = useState(null)
+
+  useEffect(() => {
+    const boardId = '69a9a1a4d73f6cadf0a87972'
+
+    fetchBoardDetailsAPI(boardId).then((board) => {
+      setBoard(board)
+    })
+  }, [])
+
   return (
     <Container disableGutters maxWidth={false} sx={{ height: '100vh'}}>
       <AppBar/>
-      <BoardBar board={mockData?.board}/>
-      <BoardContent board={mockData?.board}/>
+      <BoardBar board={board}/>
+      <BoardContent board={board}/>
     </Container>
   )
 }
