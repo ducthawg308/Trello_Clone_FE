08435a1f5aeac808a07d53195276e269bd44b88c
nguyenDucThang15 <116487174+nguyenDucThang15@users.noreply.github.com>
2026-03-11 12:59:49 +0700

    setup redux

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,3 @@
-// import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import CssBaseline from '@mui/material/CssBaseline'
@@ -6,9 +5,11 @@ import { Experimental_CssVarsProvider as CssVarsProvider } from '@mui/material/s
 import theme from './theme'
 import { ToastContainer } from 'react-toastify'
 import { ConfirmProvider } from 'material-ui-confirm'
+import { Provider } from 'react-redux'
+import { store } from '~/redux/store'
 
 createRoot(document.getElementById('root')).render(
-  // <StrictMode>
+  <Provider store={store}>
     <CssVarsProvider theme={theme}>
       <ConfirmProvider defaultOptions={{
         allowClose: false,
@@ -21,5 +22,5 @@ createRoot(document.getElementById('root')).render(
         <ToastContainer position='bottom-right'/>
       </ConfirmProvider>
     </CssVarsProvider>
-  // </StrictMode>,
+  </Provider>
 )
