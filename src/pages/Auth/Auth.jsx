e99533ec72205abca937ba380dade8c449b82f27
nguyenDucThang15 <116487174+nguyenDucThang15@users.noreply.github.com>
2026-03-12 16:23:17 +0700

    fix bg

--- src/pages/Auth/Auth.jsx
+++ src/pages/Auth/Auth.jsx
@@ -2,6 +2,7 @@ import { useLocation } from 'react-router-dom'
 import Box from '@mui/material/Box'
 import LoginForm from './LoginForm'
 import RegisterForm from './RegisterForm'
+import loginBg from '~/assets/auth/login-register-bg.jpg'
 
 function Auth() {
   const location = useLocation()
@@ -9,18 +10,20 @@ function Auth() {
   const isRegister = location.pathname === '/register'
 
   return (
-    <Box sx={{
-      display: 'flex',
-      flexDirection: 'column',
-      minHeight: '100vh',
-      alignItems: 'center',
-      justifyContent: 'flex-start',
-      background: 'url("src/assets/auth/login-register-bg.jpg")',
-      backgroundRepeat: 'no-repeat',
-      backgroundSize: 'cover',
-      backgroundPosition: 'center',
-      boxShadow: 'inset 0 0 0 2000px rgba(0, 0, 0, 0.2)'
-    }}>
+    <Box
+      sx={{
+        display: 'flex',
+        flexDirection: 'column',
+        minHeight: '100vh',
+        alignItems: 'center',
+        justifyContent: 'flex-start',
+        background: `url(${loginBg})`,
+        backgroundRepeat: 'no-repeat',
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        boxShadow: 'inset 0 0 0 2000px rgba(0,0,0,0.2)'
+      }}
+    >
       {isLogin && <LoginForm />}
       {isRegister && <RegisterForm />}
     </Box>
