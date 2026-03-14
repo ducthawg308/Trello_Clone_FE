fc281861a0724663e0fc6457960f9554e39c45a3
nguyenDucThang15 <116487174+nguyenDucThang15@users.noreply.github.com>
2026-03-14 08:58:38 +0700

    feat login, user redux

---
+++ src/redux/user/userSlice.js
@@ -0,0 +1,35 @@
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
+import authorizedAxiosInstance from '~/utils/authorizeAxios'
+import { API_ROOT } from '~/apis/environment'
+
+const initialState = {
+  currentUser: null
+}
+
+export const loginUserAPI = createAsyncThunk(
+  'user/loginUserAPI',
+  async (data) => {
+    const response = await authorizedAxiosInstance.post(`${API_ROOT}/v1/users/login`, data)
+    return response.data
+  }
+)
+
+export const userSlice = createSlice({
+  name: 'user',
+  initialState,
+  extraReducers: (builder) => {
+    builder
+      .addCase(loginUserAPI.fulfilled, (state, action) => {
+        const user = action.payload
+        state.currentUser = user
+      })
+  }
+})
+
+// export const {} = userSlice.actions
+
+export const selectCurrentUser = (state) => {
+  return state.user.currentUser
+}
+
+export const userReducer = userSlice.reducer
